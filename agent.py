5392a8d9d1f525b72a23d2c4f761996acf464c1d
Paulo Henrique <paulo_neves2@hotmail.com>
2025-09-29 23:49:57 -0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ import pandas as pd
 from langchain_openai import ChatOpenAI
 from langchain_groq.chat_models import ChatGroq
 from langchain_experimental.agents import create_pandas_dataframe_agent
+from langchain.memory import ConversationBufferMemory
 
 class CSVAnalysisAgent:
     def __init__(self, key: str):
